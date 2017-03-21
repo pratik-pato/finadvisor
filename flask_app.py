490ab62b1a2bc62f3ce83868aa7738c2822e9c6c
pratik-pato <charwad.pratik@gmail.com>
2017-03-20 19:34:44 -0500

    registration with database

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,4 @@
+import src.sql as sql
 import flask
 from flask import Flask
 app = Flask(__name__)
@@ -14,7 +15,19 @@ def register():
 @app.route('/app/dumpdata',methods = ['GET','POST'])
 def dump():
         if flask.request.method == 'POST':
+            formData = []
             result = flask.request.form
+            formData.append(result['userName'])
+            formData.append(result['contact'])
+            formData.append(result['firstName'])
+            formData.append(result['middleName'])
+            formData.append(result['lastName'])
+            formData.append(result['password'])
+            formData.append(result['email'])
+            print formData
+            if sql.consultantInsert(formData) == 1:
+                return "<h1>ERROR IN DATA</h1>"
+            #print formData[1]
             return flask.render_template('result.html',result = result)
 
 if __name__ == '__main__':
